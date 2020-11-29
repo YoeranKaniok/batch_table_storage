3b843ef449216011a374f492620f1eca78b1795d
yoeran <yka@iton.nl>
2020-11-29 09:28:56 +0100

    changed init

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as rm:
     long_description = rm.read()
 
 setuptools.setup(
-    name="table_storage_batch",
-    version="0.0.1",
+    name="batch_table_storage",
+    version="0.0.2",
     author="Yoeran Kaniok",
     author_email="yoeran@live.nl",
     description="Batch functionality for Azure Table Storage",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/YoeranKaniok/table_storage_batch",
+    url="https://github.com/YoeranKaniok/batch_table_storage",
     install_requires=[
        'azure-cosmosdb-table==1.0.6'
     ],

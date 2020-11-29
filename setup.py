2264379a5696e827da19e5095dd087ec7119d730
yoeran <yka@iton.nl>
2020-11-29 13:34:40 +0100

    version 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as rm:
 
 setuptools.setup(
     name="batch_table_storage",
-    version="0.0.7",
+    version="0.0.8",
     author="Yoeran Kaniok",
     author_email="yoeran@live.nl",
     description="Async batch functionality for Azure Table Storage",
